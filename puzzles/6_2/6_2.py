418e34ae344dfdd9153bfa4b6bb3d4cdace7792d
Patrick Fay <pfay.us@gmail.com>
2021-12-07 00:08:36 -0800

    Day 6 solution

---
+++ puzzles/6_2/6_2.py
@@ -0,0 +1,17 @@
+f = open("input.txt", "r")
+timers = [0,0,0,0,0,0,0,0,0] # 8 is max
+for line in f.readlines():
+    strippedLine = line.strip()
+    print(strippedLine)
+    for val in strippedLine.split(','):
+        timers[int(val)] += 1
+
+print(timers)
+
+for day in range(256):
+    spawners = timers.pop(0)
+    timers.append(spawners)
+    timers[6] += spawners
+    print(timers)
+
+print(sum(timers))
